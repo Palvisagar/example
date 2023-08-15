cea2381fc8a604eb023df96cbbc4382d14c20720
palvisagar <pallvisagar.code@gmail.com>
2023-08-15 19:36:12 +0530

    fisrt commit on 15th august 2023(JSX)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
+let name="palvi sagar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quam natus magni consequatur expedita odit molestias. Ea cum, 
+        dolores commodi voluptatum aperiam nemo! Laborum, necessitatibus iste.
+         Magnam libero vero asperiores eligendi!
+      </p>
     </div>
+
+    </>
   );
 }
 
